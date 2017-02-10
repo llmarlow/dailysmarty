12157f4c551ae66a7a45f1ca6c776232c6d1758b
Lorna Marlow <lornamarlow@hotmail.co.uk>
2017-02-10 14:34:52 +0000

    Add topic show page

--- spec/features/topic_spec.rb
+++ spec/features/topic_spec.rb
@@ -22,4 +22,21 @@ describe 'navigate' do
       expect(page).to have_link(@topic.title, href: topic_path(@topic))
     end
   end
+
+  describe 'show' do
+    before do
+      visit topic_path(@topic)
+    end
+    it 'can be reached successfully' do
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'should display the topic title' do
+      expect(page).to have_css('h1', text: 'Sports')
+    end
+
+    it 'should have a url that matches the custom url slug' do
+      expect(current_path).to have_content('sports')
+    end
+  end
 end
