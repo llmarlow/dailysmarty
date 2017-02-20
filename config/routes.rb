ac4d790c299f819b3b72d4ca91d594a3f93197c0
Lorna Marlow <lornamarlow@hotmail.co.uk>
2017-02-20 15:46:18 +0000

    Nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :topics
+  resources :topics do
+    scope module: :topics do
+      resources :posts
+    end
+  end
   devise_for :users, controllers: { registrations: 'registrations' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static#home'
 end
